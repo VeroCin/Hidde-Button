b4a82349f5df7b2cf992a01073a17699bd13471a
VeroCin <veronica_mircea@yahoo.com>
2024-11-09 15:06:38 +0100

    Update pe.js

--- pe.js
+++ pe.js
@@ -1,10 +1,10 @@
- let winningButtonIndex = Math.floor(Math.random() * 3);
+let winningButtonIndex = Math.floor(Math.random() * 3);
 
-        function checkButton(clickedIndex) {
-            let resultElement = document.getElementById('result');
-            if (clickedIndex === winningButtonIndex) {
-                resultElement.textContent = "castigator";
-            } else {
-                resultElement.textContent = "necastigator";
-            }
-        }
+function checkButton(clickedIndex) {
+    let resultElement = document.getElementById('result');
+    if (clickedIndex === winningButtonIndex) {
+        resultElement.textContent = "castigator";
+    } else {
+        resultElement.textContent = "necastigator";
+    }
+}
