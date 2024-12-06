23c689fb2d9a864686a7d7f48b6c9992503b4d2e
VeroCin <veronica_mircea@yahoo.com>
2024-12-06 13:23:20 +0100

    Update script1.js

--- script1.js
+++ script1.js
@@ -6,7 +6,7 @@ function checkButton(clickedIndex) {
 		resultElement.textContent = 'Winner!';
                 resultElement.style.color = 'green';
 	} else {
-                resultElement.textContent = 'Not a winner!';
+                resultElement.textContent = 'Not a winner.';
                 resultElement.style.color = 'red';
 	}
 }
@@ -17,12 +17,12 @@ function generateButtons() {
 	container.innerHTML = '';
 	document.getElementById('result').textContent = '';
 	winningButtonIndex = Math.floor(Math.random() * buttonCount);
+
 	for (let i = 0; i < buttonCount; ++i) {
 		let button = document.createElement('button');
                 button.className = 'btn btn-outline-secondary';
-                button.textContent = 'Buton' + (i + 1);
+                button.textContent = `Buton ${i + 1}`;
                 button.onclick = () => checkButton(i);
                 container.appendChild(button);
 	}
 }
-
